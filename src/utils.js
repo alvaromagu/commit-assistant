c00b31901be80b8da54ff69b6dd8f8992c55eae3
Alvaro <alvaromagu2002@gmail.com>
2023-03-02 20:08:16 +0100

    🆕 (feat): Add custom style to exitProgram

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import { outro } from '@clack/prompts'
 import colors from 'picocolors'
 
-export function exitProgram ({ code = 0, message = 'El commit ha sido cancelado' } = {}) {
-  outro(colors.yellow(message))
+export function exitProgram ({ code = 0, message = 'El commit ha sido cancelado', customStyle = false } = {}) {
+  outro(customStyle ? message : colors.yellow(message))
   process.exit(code)
 }
